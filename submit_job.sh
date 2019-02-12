baa1cc2743ae952bace9b8ad9d6f25d43f767490
L B <lb@Ls-MacBook-Pro.local>
2019-02-12 17:51:28 -0600

    bash scripts commenting'

--- submit_job.sh
+++ submit_job.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-#SBATCH -J shallow_water    # job name to display in squeue
+#
+# Single argument to 'submit_job.sh' script located in root of project's directory structure
+#        argument <output directory name>
+#        * slurn output will be written to seperate file
+#
+
+OUTPUT_DIR=$1
+
+#SBATCH -J project_1              # job name to display in squeue
 #SBATCH -o slurn_output-%j.txt    # standard output file
 #SBATCH -e slurn_error-%j.txt     # standard error file
 #SBATCH -D /users/lmbrown/scratch/parallel_scientific_computing/project_1/output
 
 #SBATCH -p htc				# requested partition
+#SBATCH --mem=7G            # memory requirment
 #SBATCH -t 180         		# kill time
 
 #SBATCH --mail-user lmbrown@smu.edu
 #SBATCH --mail-type=all
-#SBATCH --mail-type=begin
-#SBATCH --mail-type=end
 
+#
+# Dropped into '$PROJECT_DIR/output/' directory by slurn
+#
+
+# Change directory to '$PROJECT_DIR/src/'
 cd ../src
+
+# Run python 'controller.py' script passing $OUTPUT_DIR as an argument, or executable directly 'srun sw.x' 
 srun sw.x
